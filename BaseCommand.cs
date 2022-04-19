2866d0cfaf272ca1749133a2dc53d0b7cd0441e3
Ethan Ford <ethan.ford06@gmail.com>
2022-04-19 09:36:01 -0500

    Search command maybe??

--- BaseCommand.cs
+++ BaseCommand.cs
@@ -1,7 +1,9 @@
+using System.Threading.Tasks;
+
 namespace Mercurius.Commands {
     public class BaseCommand {
         public string Alias { get; private set; }
         public string Description { get; private set; }
-        public virtual void Execute() {}
+        public virtual async Task Execute(string query) {}
     }
 }
--- Commands/Search.cs
+++ Commands/Search.cs
@@ -2,13 +2,18 @@ using System.Threading;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Net.Http.Headers;
 
 namespace Mercurius.Commands {
     public class Search : BaseCommand {
-        public override void Execute(string query) {
-            
+        public override async Task Execute(string query) {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("Application/Json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "Mercurius");
 
-
+            string response = await client.GetStringAsync(@$"https://api.modrinth.com/v2/search?query={query}");
+            Console.WriteLine(response);
         }
     }
 }
