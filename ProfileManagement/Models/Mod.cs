617919ba83ef196dba57dea093838a4248870c6e
Ethan Ford <ethan.ford06@gmail.com>
2022-04-25 13:34:43 -0500

    Minor change

--- ProfileManagement/Models/Mod.cs
+++ ProfileManagement/Models/Mod.cs
@@ -4,8 +4,8 @@ namespace Mercurius.Profiles {
         public string ModrinthProjectId { get; set; }
         public string MinecraftVersion { get; set; }
         public string ModVersion { get; set; }
-        public string ServerSide { get; set; }
-        public string ClientSide { get; set; }
+        public bool ServerSideSupported { get; set; }
+        public bool ClientSideSupported { get; set; }
         public Mod[] Dependencies { get; set; }
 
     }
