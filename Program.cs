ff74d847beef42b7fb3a8f90fb2dc7ab539beb18
Kyra <ethan.ford06@gmail.com>
2022-11-30 13:56:53 -0600

    Rearranged file and updated loggin config

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NLog;
 using NLog.Config;
+using NLog.Targets;
 using NLog.Extensions.Logging;
 using Mercurius.DBus;
 using Mercurius.Configuration;
@@ -13,8 +13,22 @@ using Mercurius.Configuration;
 namespace Mercurius {
     public class Program {
         public static async Task Main(string[] args) {
-            LogManager.ThrowConfigExceptions = true;
-            LogManager.LoadConfiguration("NLog.xml");
+            ColoredConsoleTarget target = new ColoredConsoleTarget();
+            target.Layout = "[${date:format=HH\\:MM\\:ss}] ${logger} -> ${message}";
+            target.WordHighlightingRules.Add(
+                new ConsoleWordHighlightingRule("log", 
+                    ConsoleOutputColor.Cyan, 
+                    ConsoleOutputColor.NoChange));
+            target.WordHighlightingRules.Add(
+                new ConsoleWordHighlightingRule("warn", 
+                    ConsoleOutputColor.DarkYellow, 
+                    ConsoleOutputColor.NoChange));
+            target.WordHighlightingRules.Add(
+                new ConsoleWordHighlightingRule("fatal", 
+                    ConsoleOutputColor.Red,
+                    ConsoleOutputColor.Black));
+
+            NLog.Config.SimpleConfigurator.ConfigureForTargetLogging(target, LogLevel.Trace);
 
             var builder = new HostBuilder()
             .ConfigureAppConfiguration((hostingContext, config) => {
