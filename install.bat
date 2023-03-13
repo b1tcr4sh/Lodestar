069b7824310df175fadeb06f2385d881018694c7
Ethan Ford <ethan.ford06@gmail.com>
2023-03-12 22:23:39 -0500

    Probably not useable yet

---
+++ install.bat
@@ -0,0 +1,9 @@
+@REM Get latest release assets from api
+@REM Download zip
+@REM unzip into folder
+@REM cd into folder
+@REM dotnet build -r win-x64 -c Release --self-contained 
+@REM create shortcut to binary
+@REM copy shortcut to startup folder
+
+@REM How to do that? I don't fucking know windows is ass
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-echo "~Welcome Stargazer~"
+echo ~Welcome Stargazer~
 
 echo fetching build files...
 git clone https://github.com/vividuwu/lodestar
@@ -6,7 +6,17 @@ git clone https://github.com/vividuwu/lodestar
 cd lodestar
 
 echo building...
-dotnet build -c Release
+dotnet build -c Release -r linux-x64 --self-contained
 
-echo installing
-mkdir /usr/bin
+echo installing...
+mkdir /opt/lodestar/ 
+cp ./bin/Release/net7.0/linux-x64/* /opt/lodestar
+cd ../
+rm -r ./lodestar
+
+touch /usr/bin/lodestar
+echo "/opt/lodestar/Mercurius" >> /usr/bin/lodestar
+
+chmod +x /usr/bin/lodestar
+
+echo Installation Complete!
